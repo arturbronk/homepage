3520a94bcb7a01af43443ca9a8caa11394da5dec
Artur Bronk <artbronk@gmail.com>
2020-05-04 23:01:44 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const firstName='Artura';
+const product='kawy';
+
+
+
+console.log(firstName);
+console.log(product);
+console.log(`Witam na stronie ${firstName}. Weź ze sobą łyk ${product} bo bez ${product} człowiek niemrawy.`);
+alert(`Witam na stronie ${firstName}. Weź ze sobą łyk ${product} bo bez ${product} człowiek niemrawy.`);
