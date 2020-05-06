3b5172fa9a163b6f47a027d51ca75707011e18cd
Artur Bronk <artbronk@gmail.com>
2020-05-06 15:16:24 +0200

    changes to the html and ccs to center the layout and fix the navigation

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-const firstName = "Artura";
-const product = "kawy";
+// const firstName = "Artura";
+// const product = "kawy";
 
-console.log(firstName);
-console.log(product);
-console.log(
-  `Witam na stronie ${firstName}. Weź ze sobą łyk ${product} bo bez ${product} człowiek niemrawy.`
-);
-alert(
-  `Witam na stronie ${firstName}. Weź ze sobą łyk ${product} bo bez ${product} człowiek niemrawy.`
-);
+// console.log(firstName);
+// console.log(product);
+// console.log(
+//   `Witam na stronie ${firstName}. Weź ze sobą łyk ${product} bo bez ${product} człowiek niemrawy.`
+// );
+// alert(
+//   `Witam na stronie ${firstName}. Weź ze sobą łyk ${product} bo bez ${product} człowiek niemrawy.`
+// );
 
-const heading = document.querySelector ('.main-header');
-heading.innerHTML='nowa tresc'
+
 
