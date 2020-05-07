68ecb373dd773b845cb20199bd1bd82f28663dfc
Artur Bronk <artbronk@gmail.com>
2020-05-07 22:16:32 +0200

    adding week3.js for the JS test

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-// const firstName = "Artura";
-// const product = "kawy";
-
-// console.log(firstName);
-// console.log(product);
-// console.log(
-//   `Witam na stronie ${firstName}. Weź ze sobą łyk ${product} bo bez ${product} człowiek niemrawy.`
-// );
-// alert(
-//   `Witam na stronie ${firstName}. Weź ze sobą łyk ${product} bo bez ${product} człowiek niemrawy.`
-// );
+const firstName = "Artura";
+const product = "kawy";
 
 
-
+console.log(firstName);
+console.log(product);
+console.log(
+  `Witam na stronie ${firstName}. Weź ze sobą łyk ${product} bo bez ${product} człowiek niemrawy.`
+);
+alert(
+  `Witam na stronie ${firstName}. Weź ze sobą łyk ${product} bo bez ${product} człowiek niemrawy.`
+);
---
+++ week3.js
@@ -0,0 +1,7 @@
+const wordChange = "kabanos, kałasznikow";
+
+const header = document.querySelector(".main__headers--js");
+
+header.innerHTML = ` A co tu się stanęło... ${wordChange}`;
+
+console.log(header.innerHTML);
